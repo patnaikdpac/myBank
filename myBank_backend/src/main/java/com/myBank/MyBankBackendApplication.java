6a8b2b4429bd0c2e4df79796d8d918b5973ac868
DEEPAK PATNAIK <DEEPAK PATNAIK@DEEPAKPATNAIK>
2024-07-23 09:36:09 +0530

    First Commit

---
+++ myBank_backend/src/main/java/com/myBank/MyBankBackendApplication.java
@@ -0,0 +1,15 @@
+package com.myBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankBackendApplication.class, args);
+		System.out.println("This is a Mini Banking Application");
+		
+	}
+
+}
---
+++ myBank_backend/src/test/java/com/myBank/MyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
