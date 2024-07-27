7b8f79ef44896897d252b60d938efa9df099d9b5
hp <hp@LAPTOP-82T3M8DC>
2024-07-27 10:00:35 +0530

    view address and account info as response

--- myBank_backend/src/main/java/com/myBank/entity/Address.java
+++ myBank_backend/src/main/java/com/myBank/entity/Address.java
@@ -1,45 +1,60 @@
 package com.myBank.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "user_address")
+@Table(name = "address")
 public class Address implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String address_id;
+	@Column(name = "address_id")
+	private Long address_id;
 
-	@Column(name = "house_no")
-	private String houseNo;
+	@Column(name = "street", length = 100)
+	private String street;
 
-	@Column(name = "street_name")
-	private String streetName;
+	@Column(name = "city", length = 50)
+	private String city;
 
-	@Column(name = "present_address")
-	private String presentAddress;
-
-	@Column(name = "permanent_address")
-	private String permanentAddress;
-
+	@Column(name = "state", length = 50)
 	private String state;
 
-	private String district;
+	@Column(name = "zip_code", length = 10)
+	private String zipCode;
 
-	private String pincode;
+	@CreationTimestamp
+	@JsonFormat(pattern = "dd/MM/yyyy hh:mm:ss a")
+	@Column(name = "created_date")
+	private LocalDateTime creDate;
 
-	@Column(name = "user_id")
-	private Long user_id;
+	@CreatedBy
+	@Column(name = "created_user")
+	private String creUser;
 
+	@LastModifiedBy
+	private String modUser;
+
+	@UpdateTimestamp
+	@JsonFormat(pattern = "dd/MM/yyyy hh:mm:ss a")
+	private LocalDateTime modDate;
 }
--- myBank_backend/src/main/java/com/myBank/user/RegistrationServiceImpl.java
+++ myBank_backend/src/main/java/com/myBank/user/RegistrationServiceImpl.java
@@ -4,36 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.myBank.accountData.AccountInfoDao;
+import com.myBank.accountData.AddressDao;
 import com.myBank.common.ApplicationKeys;
 import com.myBank.common.EncryprDecrypt;
+import com.myBank.common.GenerateAccountNumber;
 import com.myBank.entity.AccountInfo;
+import com.myBank.entity.Address;
 import com.myBank.entity.User;
 
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
 	@Autowired
 	UserDao dao;
-	
+
 	@Autowired
 	AccountInfoDao accountInfoDao;
+	
+	@Autowired
+	AddressDao addressDao;
 
 	public User userRegistration(User user) {
-		// TO DO Existing mail/mobile check before save
+		if (dao.existsByMail(user.getMail()) || dao.existsByMobile(user.getMobile())) {
+			throw new IllegalArgumentException("Email or Mobile already exists");
+		}
 		EncryprDecrypt obj = null;
 		try {
-		obj = new EncryprDecrypt(ApplicationKeys.encKey);
+			obj = new EncryprDecrypt(ApplicationKeys.encKey);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		user.setAccountNo(GenerateAccountNumber.generateAccountNumber());
 		user.setPassword(obj.encrypt(user.getPassword()));
 		dao.save(user);
-		
-		AccountInfo accountInfo = new AccountInfo();
-		accountInfo.setAccountNo(user.getAccountNo());
-		accountInfo.setBalance(0.0);
-		accountInfo.setCreUser("SYSTEM");
-		accountInfoDao.save(accountInfo);
-		user.setAccountInfo(accountInfo);
+		if (user != null) {
+			AccountInfo accountInfo = new AccountInfo();
+			accountInfo.setAccountInfoId(user.getId());
+			accountInfo.setAccountNo(user.getAccountNo());
+			accountInfo.setBalance(0.0);
+			accountInfo.setCreUser(user.getMail());
+			accountInfoDao.save(accountInfo);
+			//user.setAccountinfo(accountInfo);
+			Address address = new Address();
+			address.setAddress_id(user.getId());
+			address.setCreUser(user.getMail());
+			address.setModUser(user.getMail());
+			addressDao.save(address);
+			//user.setAddress(address);
+		}
 		return user;
 	}
 
